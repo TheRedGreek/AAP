f8bf4c49f200f75d1cce9f3c77e127027cfad4a4
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2023-12-05 00:21:32 -0500

    test

--- filter_plugins/custom_filters.py
+++ filter_plugins/custom_filters.py
@@ -2,10 +2,10 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'extract_dict_from_list': self.extract_dict_from_list,
+            'extract_list_from_dict': self.extract_list_from_dict,
         }
 
-    def extract_dict_from_list(self, list_of_dicts, key):
+    def extract_list_from_dict(self, list_of_dicts, key):
         """
         Extracts a list of items corresponding to a given key from each dictionary in a list.
         If the input is a string, it returns the string as is.
@@ -23,6 +23,8 @@ class FilterModule(object):
         if not isinstance(list_of_dicts, list):
             raise ValueError("Input must be a list of dictionaries")
 
+        extracted_items = []
         for dict_item in list_of_dicts:
             if key in dict_item:
-                return dict_item[key]
+                extracted_items.append(dict_item[key])
+        return extracted_items
