81e4887fcb910efce916bb4914e201267cc55a84
greek_fire <57952350+TheRedGreek@users.noreply.github.com>
2023-12-07 12:05:06 -0500

    tt

--- filter_plugins/custom_filters.py
+++ filter_plugins/custom_filters.py
@@ -2,10 +2,11 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'extract_list_from_dict': self.extract_list_from_dict,
+            'extract_dict': self.extract_dict,
+            'extract_list': self.extract_list
         }
-
-    def extract_list_from_dict(self, list_of_dicts, key):
+    
+    def extract_list(self, item, key):
         """
         Extracts a list of items corresponding to a given key from each dictionary in a list.
         If the input is a string, it returns the string as is.
@@ -15,16 +16,56 @@ class FilterModule(object):
         Returns:
             A list of extracted items, or the original string if the input is a string.
         """
-        # Return the input as is if it's a string
-        if isinstance(list_of_dicts, str):
-            return list_of_dicts
+        # Proceed with the original logic if the input is a list
+        if not isinstance(item, dict):
+            raise ValueError("Input must be a dictionary !!!")
+        
+        if not isinstance(key, str):
+            raise ValueError("Key must be a string !!!")
+
+        if item.get(key) is None:
+            raise ValueError(f'Your value: "{key}" not found in dictionary !!!')       
+        
+        extracted_items = []
+        list_of_dicts = item.get(key)
+
+        for dic in list_of_dicts:
+            org = dic.get('organization')
+            if 'organization' not in dic:
+                dic['organization'] = org
+            extracted_items.append(dic)
+        
+        return extracted_items
+
+    def extract_dict(self, dic, key):
+        """
+        Extracts a list of items corresponding to a given key from each dictionary in a list.
+        If the input is a string, it returns the string as is.
+        Args:
+            list_of_dicts: A list of dictionaries or a string.
+            key: The key to extract items for.
+        Returns:
+            A list of extracted items, or the original string if the input is a string.
+        """
 
         # Proceed with the original logic if the input is a list
-        if not isinstance(list_of_dicts, list):
-            raise ValueError("Input must be a list of dictionaries")
-
+        if not isinstance(dic, dict):
+            raise ValueError("Input must be a dictionary")
+        
+        if not isinstance(key, str):
+            raise ValueError("Key must be a string")        
+        
         extracted_items = []
-        for dict_item in list_of_dicts:
-            if key in dict_item:
-                extracted_items.append(dict_item[key])
+        org = dic.get('organization')
+
+        if key not in dic:
+            raise ValueError(f'Your value: "{key}" not found in dictionary')
+
+        if isinstance(dic[key], dict):
+            if 'organization' not in dic[key]:
+                dic[key]['organization'] = org
+            extracted_items.append(dic[key])
+        else:
+            raise ValueError(f'Your value: "{key}" is not dictionary')
+        
         return extracted_items
